53d84fef0374ef9cf547997898c7c610ca51b892
Joe Moceri <jmoceri34@gmail.com>
2019-12-14 22:42:29 -0500

    changes
    changes

---
+++ sql-schema-compare.ps1
@@ -0,0 +1,17 @@
+param(
+	[string]$msbuildPath = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe",
+	[string]$projectPath,
+	[string]$visualStudioVersion = "16.0",
+	[Parameter(Mandatory=$true)][string]$source,
+	[Parameter(Mandatory=$true)][string]$target,
+	[Parameter(Mandatory=$true)][string]$xmlOutputPath
+)
+
+if (-not [string]::IsNullOrWhiteSpace($projectPath))
+{
+	$projectPath = Resolve-Path *.sqlproj | select -ExpandProperty Path
+}
+
+$args = @("$projectPath", "/t:SqlSchemaCompare", "/p:VisualStudioVersion=$visualStudioVersion", "/p:source=`"$source`"", "/p:target=`"$target`"", "/p:XmlOutput=`"$xmlOutputPath`"");
+
+& $msbuildPath $args
